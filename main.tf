82540c56ca0f482d7d7dc1706381404f7ebbbe45
Mehdi <mm.nikfarjam@gmail.com>
2026-03-26 12:00:04 +0000

    Use compute, network and finops module

--- main.tf
+++ main.tf
@@ -3,3 +3,18 @@ module "finops" {
   billing_account_id = var.billing_account_id
   cloud_budget       = var.cloud_budget
 }
+
+module "network" {
+  source   = "./network"
+  region   = var.region
+  vpc_cidr = var.vpc_cidr
+}
+
+module "workspace" {
+  source          = "./compute"
+  region          = var.region
+  subnet_id       = module.network.subnet_id
+  vm_type         = var.vm_type
+  vm_image        = var.vm_image
+  vm_storage_size = var.vm_storage_size
+}
